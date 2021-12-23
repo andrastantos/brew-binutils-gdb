458b0cef2ad06d81b2ebf2812ac641baf9028308
Andras Tantos <andras@tantosonline.com>
2021-12-22 17:18:38 -0800

    First few instructions work in disassembler.

--- opcodes/brew-dis.c
+++ opcodes/brew-dis.c
@@ -30,202 +30,158 @@
 #include "opcode/brew.h"
 #include "disassemble.h"
 
+#define DEBUG(...) { fprintf (stderr, __VA_ARGS__); fprintf(stderr, "\n"); }
+//#define DEBUG(...)
+
 static fprintf_ftype fpr;
 static void *stream;
+static struct disassemble_info * info;
+static bfd_vma addr;
 
 /* Macros to extract operands from the instruction word.  */
-#define OP_A(i) ((i >> 4) & 0xf)
-#define OP_B(i) (i & 0xf)
-#define INST2OFFSET(o) (((((o) & 0x3ff) ^ 0x200) - 0x200) * 2)
+#define FIELD_C ((inst_code >> 12) & 0xf)
+#define FIELD_B ((inst_code >> 8) & 0xf)
+#define FIELD_A ((inst_code >> 4) & 0xf)
+#define FIELD_D ((inst_code >> 0) & 0xf)
+
+#define REG_D (reg_names[FIELD_D])
+#define REG_A (reg_names[FIELD_A])
+#define REG_B (reg_names[FIELD_B])
 
 static const char * reg_names[16] =
-  { "$fp", "$sp", "$r0", "$r1", "$r2", "$r3", "$r4", "$r5",
-    "$r6", "$r7", "$r8", "$r9", "$r10", "$r11", "$r12", "$r13" };
+  { "$pc", "$r1",  "$r2",  "$r3",  "$r4", "$r5", "$r6",  "$r7", 
+    "$r8", "$r9", "$r10", "$r11", "$r12", "$r13" "$r14", "<<<INVALID>>>"};
+
+static bool get_uint16(uint16_t *buffer)
+{
+  int status;
+
+  if ((status = info->read_memory_func(addr, (bfd_byte *)buffer, 2, info)))
+    {
+      info->memory_error_func (status, addr, info);
+      return false;
+    }
+  addr += 2;
+  *buffer = bfd_getl16(buffer);
+  return true;
+}
+
+static bool get_uint32(uint32_t *buffer)
+{
+  int status;
+
+  if ((status = info->read_memory_func(addr, (bfd_byte *)buffer, 4, info)))
+    {
+      info->memory_error_func (status, addr, info);
+      return false;
+    }
+  addr += 4;
+  *buffer = bfd_getl32(buffer);
+  return true;
+}
+
+#define ERR_RETURN { info->memory_error_func(0, addr, info); }
 
 int
-print_insn_brew (bfd_vma addr, struct disassemble_info * info)
+print_insn_brew (bfd_vma addrP, struct disassemble_info * infoP)
 {
   int length = 2;
-  int status;
+  uint16_t inst_code;
+  uint32_t field_e;
+
+  OPCODES_ASSERT(infoP->endian == BFD_ENDIAN_LITTLE);
+
+  addr = addrP;
+  info = infoP;
   stream = info->stream;
-  const brew_opc_info_t * opcode;
-  bfd_byte buffer[4];
-  unsigned short iword;
   fpr = info->fprintf_func;
 
-  if ((status = info->read_memory_func (addr, buffer, 2, info)))
-    goto fail;
 
-  if (info->endian == BFD_ENDIAN_BIG)
-    iword = bfd_getb16 (buffer);
-  else
-    iword = bfd_getl16 (buffer);
-
-  /* Form 1 instructions have the high bit set to 0.  */
-  if ((iword & (1<<15)) == 0)
+  if (!get_uint16(&inst_code))
     {
-      /* Extract the Form 1 opcode.  */
-      opcode = &brew_form1_opc_info[iword >> 8];
-      switch (opcode->itype)
-	{
-	case BREW_F1_NARG:
-	  fpr (stream, "%s", opcode->name);
-	  break;
-	case BREW_F1_A:
-	  fpr (stream, "%s\t%s", opcode->name,
-	       reg_names[OP_A(iword)]);
-	  break;
-	case BREW_F1_AB:
-	  fpr (stream, "%s\t%s, %s", opcode->name,
-	       reg_names[OP_A(iword)],
-	       reg_names[OP_B(iword)]);
-	  break;
-	case BREW_F1_A4:
-	  {
-	    unsigned imm;
-	    if ((status = info->read_memory_func (addr + 2, buffer, 4, info)))
-	      goto fail;
-	    if (info->endian == BFD_ENDIAN_BIG)
-	      imm = bfd_getb32 (buffer);
-	    else
-	      imm = bfd_getl32 (buffer);
-	    fpr (stream, "%s\t%s, 0x%x", opcode->name,
-		 reg_names[OP_A(iword)], imm);
-	    length = 6;
-	  }
-	  break;
-	case BREW_F1_4:
-	  {
-	    unsigned imm;
-	    if ((status = info->read_memory_func (addr + 2, buffer, 4, info)))
-	      goto fail;
-	    if (info->endian == BFD_ENDIAN_BIG)
-	      imm = bfd_getb32 (buffer);
-	    else
-	      imm = bfd_getl32 (buffer);
-	    fpr (stream, "%s\t0x%x", opcode->name, imm);
-	    length = 6;
-	  }
-	  break;
-	case BREW_F1_M:
-	  {
-	    unsigned imm;
-	    if ((status = info->read_memory_func (addr + 2, buffer, 4, info)))
-	      goto fail;
-	    if (info->endian == BFD_ENDIAN_BIG)
-	      imm = bfd_getb32 (buffer);
-	    else
-	      imm = bfd_getl32 (buffer);
-	    fpr (stream, "%s\t", opcode->name);
-	    info->print_address_func ((bfd_vma) imm, info);
-	    length = 6;
-	  }
-	  break;
-	case BREW_F1_AiB:
-	  fpr (stream, "%s\t(%s), %s", opcode->name,
-	       reg_names[OP_A(iword)], reg_names[OP_B(iword)]);
-	  break;
-	case BREW_F1_ABi:
-	  fpr (stream, "%s\t%s, (%s)", opcode->name,
-	       reg_names[OP_A(iword)], reg_names[OP_B(iword)]);
-	  break;
-	case BREW_F1_4A:
-	  {
-	    unsigned imm;
-	    if ((status = info->read_memory_func (addr + 2, buffer, 4, info)))
-	      goto fail;
-	    if (info->endian == BFD_ENDIAN_BIG)
-	      imm = bfd_getb32 (buffer);
-	    else
-	      imm = bfd_getl32 (buffer);
-	    fpr (stream, "%s\t0x%x, %s",
-		 opcode->name, imm, reg_names[OP_A(iword)]);
-	    length = 6;
-	  }
-	  break;
-	case BREW_F1_AiB2:
-	  {
-	    unsigned imm;
-	    if ((status = info->read_memory_func (addr+2, buffer, 2, info)))
-	      goto fail;
-	    if (info->endian == BFD_ENDIAN_BIG)
-	      imm = bfd_getb16 (buffer);
-	    else
-	      imm = bfd_getl16 (buffer);
-	    fpr (stream, "%s\t0x%x(%s), %s", opcode->name,
-		 imm,
-		 reg_names[OP_A(iword)],
-		 reg_names[OP_B(iword)]);
-	    length = 4;
-	  }
-	  break;
-	case BREW_F1_ABi2:
-	  {
-	    unsigned imm;
-	    if ((status = info->read_memory_func (addr+2, buffer, 2, info)))
-	      goto fail;
-	    if (info->endian == BFD_ENDIAN_BIG)
-	      imm = bfd_getb16 (buffer);
-	    else
-	      imm = bfd_getl16 (buffer);
-	    fpr (stream, "%s\t%s, 0x%x(%s)",
-		 opcode->name,
-		 reg_names[OP_A(iword)],
-		 imm,
-		 reg_names[OP_B(iword)]);
-	    length = 4;
-	  }
-	  break;
-        case BREW_BAD:
-	  fpr (stream, "bad");
-	  break;
-	default:
-	  abort();
-	}
+      return -1;
     }
-  else if ((iword & (1<<14)) == 0)
+  /* Determine if we need field_e and read it if we do */
+  if (FIELD_D == 0xf || FIELD_B == 0xf || FIELD_A == 0xf || (inst_code >> 11) == 0x1f) {
+    length = 6;
+    if (!get_uint32(&field_e))
+      {
+        return -1;
+      }
+  }
+  switch (FIELD_C)
     {
-      /* Extract the Form 2 opcode.  */
-      opcode = &brew_form2_opc_info[(iword >> 12) & 3];
-      switch (opcode->itype)
-	{
-	case BREW_F2_A8V:
-	  fpr (stream, "%s\t%s, 0x%x",
-	       opcode->name,
-	       reg_names[(iword >> 8) & 0xf],
-	       iword & ((1 << 8) - 1));
-	  break;
-	case BREW_F2_NARG:
-	  fpr (stream, "%s", opcode->name);
-	  break;
-        case BREW_BAD:
-	  fpr (stream, "bad");
-	  break;
-	default:
-	  abort();
-	}
+    case 0x0: /* ^ and special */
+      switch (inst_code)
+        {
+          case 0x0000:
+            fpr(stream, "FILL");
+            return length;
+          case 0x0110:
+            fpr(stream, "BREAK");
+            return length;
+          case 0x0220:
+            fpr(stream, "SYSCALL");
+            return length;
+          case 0x0330:
+            fpr(stream, "STU");
+            return length;
+          case 0x0440:
+            fpr(stream, "SII");
+            return length;
+          case 0x0dd0:
+            fpr(stream, "FENCE");
+            return length;
+          case 0x0ee0:
+            fpr(stream, "WFENCE");
+            return length;
+        }
+      if ((inst_code & 0xfff0) == 0x0ff0)
+        {
+          /* load immedate */
+          fpr(stream, "%s <- %d", REG_D, field_e);
+        }
+      break;
+    case 0x1:
+    break;
+    case 0x2:
+    break;
+    case 0x3:
+    break;
+    case 0x4:
+    break;
+    case 0x5:
+    break;
+    case 0x6:
+    break;
+    case 0x7:
+    break;
+    case 0x8:
+    break;
+    case 0x9:
+    break;
+    case 0xa:
+    break;
+    case 0xb:
+    break;
+    case 0xc:
+    break;
+    case 0xd:
+    break;
+    case 0xe:
+    break;
+    case 0xf:
+    break;
+    default:
+      OPCODES_ASSERT(false);
+    }
+  if (length == 2)
+    {
+      fpr(stream, "unknown instruction 0x%04x", inst_code);
     }
   else
     {
-      /* Extract the Form 3 opcode.  */
-      opcode = &brew_form3_opc_info[(iword >> 10) & 15];
-      switch (opcode->itype)
-	{
-	case BREW_F3_PCREL:
-	  fpr (stream, "%s\t", opcode->name);
-	  info->print_address_func (addr + INST2OFFSET (iword) + 2, info);
-	  break;
-        case BREW_BAD:
-	  fpr (stream, "bad");
-	  break;
-	default:
-	  abort();
-	}
+      fpr(stream, "unknown instruction 0x%04x 0x%08x", inst_code, field_e);
     }
-
   return length;
-
- fail:
-  info->memory_error_func (status, addr, info);
-  return -1;
 }
