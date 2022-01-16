691abe3430e9577e285decc25874e0dbd3570048
Andras Tantos <andras@tantosonline.com>
2022-01-15 17:03:29 -0800

    Much more complete ALU reg-to-reg testing.

---
+++ gas/testsuite/gas/brew/test_alu_rr.s
@@ -0,0 +1,26 @@
+  .text
+  .p2align 1
+  # register-to-register ALU operations
+  .irp A,1,2,3,4,5,6,7,8,9,10,11,12,13,14
+  .irp B,1,2,3,4,5,6,7,8,9,10,11,12,13,14
+  .irp D,1,2,3,4,5,6,7,8,9,10,11,12,13,14
+  #.irp A,1
+  #.irp B,1
+  #.irp D,1
+  .if \A != \B
+  $r\D <- $r\A ^ $r\B
+  .endif
+  $r\D <- $r\A | $r\B
+  $r\D <- $r\A & $r\B
+  $r\D <- $r\A - $r\B
+  $r\D <- $r\A + $r\B
+  $r\D <- $r\A << $r\B
+  $r\D <- $r\A >> $r\B
+  $sr\D <- $sr\A >> $r\B
+  $r\D <- $r\A * $r\B
+  $sr\D <- $sr\A * $sr\B
+  $r\D <- upper $r\A * $r\B
+  $sr\D <- upper $sr\A * $sr\B
+  .endr
+  .endr
+  .endr
